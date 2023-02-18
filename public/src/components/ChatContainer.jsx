393732f585e901f052bd00df23062957851247a1
LiranBashari <liranbashari1@gmail.com>
2023-02-19 00:56:10 +0200

    input ui is ready

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import styled from "styled-components";
+import ChatInput from "./ChatInput";
 
-function ChatContainer({currentUser,currentContact}) {
+function ChatContainer({ currentUser, currentContact }) {
     return (
-        <div>
-            {currentContact.username}
-        </div>
+        <Container>
+            <div className="container-header">
+                <h3>{currentContact.username}</h3>
+                <hr />
+            </div>
+            <ChatInput/>
+        </Container>
     );
 }
+const Container = styled.div`
+  background-color: rgba(255, 255, 255, 0.87);
+  width: 100%;
+  text-align: center;
+  
+  .container-header{
+    h3 {
+      margin: 0;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    hr {
+      margin: 10px auto;
+      border: none;
+      border-top: 4px solid #ccc;
+      width: 100%;
+      box-sizing: content-box;
+    }
+  }
+
+`;
 
 export default ChatContainer;
